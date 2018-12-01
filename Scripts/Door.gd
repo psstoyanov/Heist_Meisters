161dc3656913e0d2929e53ef4a59d2641b9c28d6
PakoSt <kokotekilata@gmail.com>
2018-12-01 21:03:29 +0000

    S4L59 - Making a Simple Door
    - Create a simple door scene
    - Automatically opens for NPCs
    - Player has to click to open the door
    - Add sound effect via the AnimationPlayer node

---
+++ Scripts/Door.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var can_click = false
+
+
+func _on_Door_body_entered(body):
+	if not body == Global.Player and not $AnimationPlayer.is_playing():
+		open()
+	else:
+		can_click = true
+
+
+func _on_Door_body_exited(body):
+	if not $AnimationPlayer.is_playing():
+		if body == Global.Player:
+			can_click = false
+
+
+func open():
+	$AnimationPlayer.play("open")
+
+
+func _input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		open()
